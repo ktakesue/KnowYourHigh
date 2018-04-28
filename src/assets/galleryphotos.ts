17fe96f20fcfa7dca1de88d91fdd44d2a78e5fce
Audrey <audreybrown94@gmail.com>
2018-04-28 10:57:36 -1000

    gallery rendering images

---
+++ src/assets/galleryphotos.ts
@@ -0,0 +1,10 @@
+export const PHOTOS = [
+  'https://coopervision.com/sites/coopervision.com/files/styles/cv_blog_large/public/blog-post-images/3794845.jpg?itok=zzE-YISP&timestamp=1414590345',
+  'http://cdn.playbuzz.com/cdn/21fefeec-71ee-4325-8aa4-e22bfe6d2f0a/da28c0b7-ba32-4126-8e7f-c25d2fc65075_560_420.jpg',
+  'https://img.playbuzz.com/image/upload/c_crop,h_479,w_639,x_1,y_0/f_auto,fl_lossy,q_auto/c_limit,w_640/v1484183052/g3awxaoruvlvbnqqrlg7.jpg',
+  'http://brainden.com/images/static1-big.jpg',
+  'https://i.ytimg.com/vi/7hrofj5te88/maxresdefault.jpg',
+  'https://steemit-production-imageproxy-upload.s3.amazonaws.com/DQmbopwPG1xrCge8PLEPUuZvsU8hqdwajfmdP92D3jxWHjs',
+  'https://images.fatherly.com/wp-content/uploads/2016/09/optical-illusion-scintillating-grid-illusion.jpg',
+  'http://www.illusionspoint.com/wp-content/uploads/2015/06/Cube-Background.jpg'
+];
